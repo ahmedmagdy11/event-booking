d35d267166e7e1ccc2dd94dd90aa78b91459f6ae
unknown <ahmed.magdy.9611@gmail.com>
2020-06-09 02:37:58 +0200

    setting up query for GQ api

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
-const express = require('express')
-const graphQlHttp= require('express-graphql')
-const {buildSchema} = require('graphql')
-const app = express()
+const express = require("express");
+const graphQlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const app = express();
 
+let events = [];
 
-app.use('/graphql',graphQlHttp({
-    schema:buildSchema(`
+app.use(
+  "/graphql",
+  graphQlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID 
+            title : String! 
+            description: String!
+            price : Float!
+            date : String!
+        }
+        input eventInput{
+            _id: ID 
+            title : String! 
+            description: String!
+            price : Float!
+            date : String!
+        }
         type RootQuery {
-            events : [String!]!
+            events : [Event!]!
         }
         type RootMutation{
-            createEvent(name : String):String  
+            createEvent(arguments:eventInput):Event  
         }
         schema {
             query: RootQuery
             mutation: RootMutation
         }
     `),
-    rootValue:{
-        events : ()=>{
-            return [..."my cool string"]
-        },
-        createEvent:(args)=>{
-            return args.name
-        }
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+          args = args.arguments
+          console.log(args)
+          const arg_events = {
+            _id: Math.random.toString,
+            name: args.name,
+            title: args.title,
+            description: args.description,
+            price: +args.price,
+            date: new Date().toISOString(),
+          }
+          events.push(arg_events)
+        return arg_events
+      }, 
     },
-    //remove in production 
-    graphiql:true
-}))
+    //remove in production
+    graphiql: true,
+  })
+);
 
-app.listen(3000,()=>{
-    console.log(`app listening on port ${3000}`)
-})
+app.listen(3000, () => {
+  console.log(`app listening on port ${3000}`);
+});
